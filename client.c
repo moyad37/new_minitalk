2c0a6ecf41b961dbb40c5f0702d32ff00a503090
mouayad37 <mouayad2003mansour@gmail.com>
2023-06-25 17:11:49 +0200

    new_mini

--- client.c
+++ client.c
@@ -1,106 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmanssou <mmanssou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 17:09:07 by mmanssou          #+#    #+#             */
+/*   Updated: 2023/06/25 17:11:12 by mmanssou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
-#include "libft/libft.h"
 
-
-char *convert_char_to_binary(char c)
+char	*convert_char_to_binary(char c)
 {
-    int dec;
-    int i;
-    char *binary;
-    int bit;
+	int		dec;
+	int		i;
+	char	*binary;
+	int		bit;
 
-    i = 7;
-    dec = (int)c;
-    binary = malloc(sizeof(char) * 9);
-    if (binary == NULL)
-    {
-        fprintf(stderr, "Error: Memory allocation failed.\n");
-        exit(1);
-    }
-    while (i >= 0)
-    {
-        bit = (dec >> i) & 1;
-        binary[7 - i] = bit + '0';
-        i--;
-    }
-    binary[8] = '\0';
-    return binary;
+	i = 7;
+	dec = (int)c;
+	binary = malloc(sizeof(char) * 9);
+	if (binary == NULL)
+	{
+		write(1, "memory error\n", 13);
+		exit(1);
+	}
+	while (i >= 0)
+	{
+		bit = (dec >> i) & 1;
+		binary[7 - i] = bit + '0';
+		i--;
+	}
+	binary[8] = '\0';
+	return (binary);
 }
 
-void send_binary(char *binary, pid_t pid)
+void	send_binary(char *binary, pid_t pid)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i <= 7)
-    {
-        if (binary[i] == '0')
-        {
-            kill(pid, SIGUSR1);
-            usleep(300);
-        }
-        else if (binary[i] == '1')
-        {
-            kill(pid, SIGUSR2);
-            usleep(300);
-        }
-        i++;
-    }
+	i = 0;
+	while (i <= 7)
+	{
+		if (binary[i] == '0')
+		{
+			kill(pid, SIGUSR1);
+			usleep(300);
+		}
+		else if (binary[i] == '1')
+		{
+			kill(pid, SIGUSR2);
+			usleep(300);
+		}
+		i++;
+	}
 }
 
-void send_null(pid_t pid)
+void	send_null(pid_t pid)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i != 8)
-    {
-        kill(pid, SIGUSR1);
-        usleep(300);
-        i++;
-    }
+	i = 0;
+	while (i != 8)
+	{
+		kill(pid, SIGUSR1);
+		usleep(300);
+		i++;
+	}
 }
 
-void handle_av(const char *av, pid_t pid)
+void	handle_av(const char *av, pid_t pid)
 {
-    int i;
-    char *binary;
+	int		i;
+	char	*binary;
 
-    i = 0;
-    while (av[i] != '\0')
-    {
-        binary = convert_char_to_binary(av[i]);
-        send_binary(binary, pid);
-        free(binary);
-        i++;
-    }
-    send_null(pid);
+	i = 0;
+	while (av[i] != '\0')
+	{
+		binary = convert_char_to_binary(av[i]);
+		send_binary(binary, pid);
+		free(binary);
+		i++;
+	}
+	send_null(pid);
 }
-void print_ok(int sig)
+
+int	main(int ac, char **av)
 {
-        (void)sig;
-        write(1, "ok\n", 3);
-}
-int main(int ac, char **av)
-{
-    if (ac != 3)
-    {
-        write(2, "ERROR\n", 6);
-        return 0;
-    }
-    else
-    {
-        if (!*av[2])
-            return 0;
-        else
-        {
-            handle_av(av[2], atoi(av[1]));
-            //usleep(200);
-            //signal(SIGUSR1, print_ok);
-        }
-    }
-    return 0;
+	if (ac != 3)
+	{
+		write(2, "ERROR\n", 6);
+		return (0);
+	}
+	else
+	{
+		if (!*av[2])
+			return (0);
+		else
+		{
+			handle_av(av[2], atoi(av[1]));
+		}
+	}
+	return (0);
 }
